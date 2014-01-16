89f0277465e141a124367480ca32ec040babb735
Jan Dittberner <jan@dittberner.info>
2014-01-16 21:08:20 +0100

    initial project setup

---
+++ README.rst
@@ -0,0 +1,22 @@
+==============================================
+pkiutils - Public Key Infrastructure Utilities
+==============================================
+
+This is a set of pycrypto_ based tools to create, load and verify public key
+infrastructure material like:
+
+* RSA keys
+* X.509 certificates
+* X.509 certificate bundles from files or directories
+
+.. _pycrypto: https://pypi.python.org/pypi/pycrypto/
+
+Installation
+============
+
+python setup.py install
+
+License
+=======
+
+The pkiutils package is licensed under the terms of the MIT license.
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Setup for pkiutils.
+"""
+
+__author__ = 'Jan Dittberner'
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst', 'r') as readme:
+    DESCRIPTION = readme.read()
+
+DESCRIPTION += "\n"
+
+with open('LICENSE', 'r') as license:
+    DESCRIPTION += license.read()
+
+
+setup(
+    name='pkiutils',
+    description='a set of public key infrastructure utilities',
+    long_description=DESCRIPTION,
+    install_requires=['pycrypto'],
+    version='0.1',
+    author=__author__,
+    author_email='jan@dittberner.info',
+    packages=find_packages(),
+    license='MIT',
+)
